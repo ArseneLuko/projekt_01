44a4d6ddbff21d30e2deed03570092591629677a
ArseneLuko <arsene.luko@gmail.com>
2024-07-12 16:46:45 +0200

    Přidán výběr textu.

--- projekt_01.py
+++ projekt_01.py
@@ -5,6 +5,7 @@ email: lukas@lukaskarasek.com
 discord: lukaskarasek__77224
 """
 # importy
+from task_template import TEXTS
 
 # proměnné
 uzivatele = {
@@ -24,7 +25,6 @@ def zadej_udaje():
     heslo = input("Zadej heslo: ")
     return (jmeno, heslo)
 
-
 def je_registrovany(jmeno, heslo):
     """
     Tato funkce vrátí True, jestliže je uživatel registrovaný
@@ -33,13 +33,31 @@ def je_registrovany(jmeno, heslo):
         return True
     else:
         return False
+    
+def vyber_text():
+    while True:
+        vstup = input("Který text chceš analyzovat? Zadej číslo 1 - 3: ")
+        
+        try: # otestuje, jestli je možné převést vstup na celé číslo
+            vstup = int(vstup)
+        except ValueError:
+            print("Je potřeba zadat celé číslo")
+            continue
 
+        # kontrola jestli je číslo v rozmezí 1 až 3
+        if vstup not in range(1,4):
+            print("Zadané číslo musí být v rozmezí 1 - 3.")
+            continue
+
+        return vstup
 
 # hlavní pogram
 if __name__ == "__main__":
-    jmeno, heslo = zadej_udaje()
+    #jmeno, heslo = zadej_udaje()
+    jmeno, heslo = "bob", "123" # slouží při testování k vynechání zadávání
     if je_registrovany(jmeno, heslo):
-        print(f"{jmeno}, vítej v aplikaci")
+        print(f"{jmeno}, vítej v aplikaci,\nk analýze máme 3 texty.")
+        k_analyze = vyber_text()
+        print(f"Budeme analyzovat text číslo {k_analyze}")
     else:
         print("Uživatel nenalezen nebo zadáno špatné heslo. Ukončuji program")
-        
