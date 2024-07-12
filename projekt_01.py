dbae6a8695b9e82899c100f0ace780081dcee2d6
ArseneLuko <arsene.luko@gmail.com>
2024-07-12 17:03:31 +0200

    Ubrány funkce, kde nejsou potřeba a zjednodušen kód

--- projekt_01.py
+++ projekt_01.py
@@ -16,24 +16,6 @@ uzivatele = {
 }
 
 # definice funkcí
-
-def zadej_udaje():
-    """
-    Funkce si vyžádá od uživatele dvy vstupy, uživ. jméno a heslo. Ty pak vrátí na výstupu jako Tuple
-    """
-    jmeno = input("Zadej uživatelské jméno: ")
-    heslo = input("Zadej heslo: ")
-    return (jmeno, heslo)
-
-def je_registrovany(jmeno, heslo):
-    """
-    Tato funkce vrátí True, jestliže je uživatel registrovaný
-    """
-    if (jmeno in uzivatele) and (heslo == uzivatele[jmeno]):
-        return True
-    else:
-        return False
-    
 def vyber_text():
     while True:
         vstup = input("Který text chceš analyzovat? Zadej číslo 1 - 3: ")
@@ -41,23 +23,28 @@ def vyber_text():
         try: # otestuje, jestli je možné převést vstup na celé číslo
             vstup = int(vstup)
         except ValueError:
-            print("Je potřeba zadat celé číslo")
-            continue
+            print("Je potřeba zadat celé číslo. Ukončuji program")
+            break
 
         # kontrola jestli je číslo v rozmezí 1 až 3
         if vstup not in range(1,4):
-            print("Zadané číslo musí být v rozmezí 1 - 3.")
-            continue
+            print("Zadané číslo bělo být v rozmezí 1 - 3. Ukončuji program")
+            break
 
-        return vstup
+        return vstup # pomocí return se ukončí nekonečná smyčka while True
 
 # hlavní pogram
 if __name__ == "__main__":
-    #jmeno, heslo = zadej_udaje()
+    # jmeno = input("Zadej uživatelské jméno: ")
+    # heslo = input("Zadej heslo: ")
     jmeno, heslo = "bob", "123" # slouží při testování k vynechání zadávání
-    if je_registrovany(jmeno, heslo):
+
+    # otestujeme, jestli uživatel existuje a je zadáno odpovídající heslo
+    if (jmeno in uzivatele) and (heslo == uzivatele[jmeno]):
         print(f"{jmeno}, vítej v aplikaci,\nk analýze máme 3 texty.")
         k_analyze = vyber_text()
         print(f"Budeme analyzovat text číslo {k_analyze}")
+        
     else:
         print("Uživatel nenalezen nebo zadáno špatné heslo. Ukončuji program")
+
