481507fd00ca96daf006f8dd0304a8af1f49ab57
ArseneLuko <arsene.luko@gmail.com>
2024-07-12 17:12:09 +0200

    zmenšení kódu

--- projekt_01.py
+++ projekt_01.py
@@ -16,22 +16,6 @@ uzivatele = {
 }
 
 # definice funkcí
-def vyber_text():
-    while True:
-        vstup = input("Který text chceš analyzovat? Zadej číslo 1 - 3: ")
-        
-        try: # otestuje, jestli je možné převést vstup na celé číslo
-            vstup = int(vstup)
-        except ValueError:
-            print("Je potřeba zadat celé číslo. Ukončuji program")
-            break
-
-        # kontrola jestli je číslo v rozmezí 1 až 3
-        if vstup not in range(1,4):
-            print("Zadané číslo bělo být v rozmezí 1 - 3. Ukončuji program")
-            break
-
-        return vstup # pomocí return se ukončí nekonečná smyčka while True
 
 # hlavní pogram
 if __name__ == "__main__":
@@ -42,7 +26,20 @@ if __name__ == "__main__":
     # otestujeme, jestli uživatel existuje a je zadáno odpovídající heslo
     if (jmeno in uzivatele) and (heslo == uzivatele[jmeno]):
         print(f"{jmeno}, vítej v aplikaci,\nk analýze máme 3 texty.")
-        k_analyze = vyber_text()
+        k_analyze = input("Který text chceš analyzovat? Zadej číslo 1 - 3: ")
+        
+        # otestuje, jestli je možné převést vstup na celé číslo
+        try:
+            k_analyze = int(k_analyze)
+        except ValueError:
+            print("Je potřeba zadat celé číslo. Ukončuji program")
+            quit()
+
+        # kontrola jestli je číslo v rozmezí 1 až 3
+        if k_analyze not in range(1,4):
+            print("Zadané číslo bělo být v rozmezí 1 - 3. Ukončuji program")
+            quit()
+        
         print(f"Budeme analyzovat text číslo {k_analyze}")
         
     else:
